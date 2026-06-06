9df03c5c8db52c2df95a73c6db7faace05b36c8a
Val-d-emar <vhot@live.ru>
2026-06-06 14:17:26 +0400

    fix: cart.module.ts

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -4,10 +4,17 @@ import { OrderModule } from '../order/order.module';
 
 import { CartController } from './cart.controller';
 import { CartService } from './services';
+import { CartEntity } from './entities/cart.entity';
+import { CartItemEntity } from './entities/cart-item.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    OrderModule,
+    TypeOrmModule.forFeature([CartEntity, CartItemEntity]),
+  ],
   providers: [CartService],
   controllers: [CartController],
+  exports: [CartService],
 })
 export class CartModule {}
